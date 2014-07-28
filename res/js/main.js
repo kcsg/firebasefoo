aa4b7e2b5ccccc96835d4018db845d04c90efa7a
Genji Noguchi <genji.noguchi@gmail.com>
2014-07-28 01:26:58 -0400

    tiny things committed

--- res/js/main.js
+++ res/js/main.js
@@ -1,13 +1,6 @@
 $(document).ready(function(){
 	init();
 
-	var http = require('http');
-	http.createServer(function (req, res) {
-	  res.writeHead(200, {'Content-Type': 'text/plain'});
-	  res.end('Hello Node.js\n');
-	}).listen(8124, "127.0.0.1");
-	console.log('Server running at http://127.0.0.1:8124/');
-
 
 });
 
